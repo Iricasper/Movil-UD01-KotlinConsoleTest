71de99e029b4e83d34d9f539bec7b01031f367df
Iris <acasper631@g.educaand.es>
2025-10-07 14:40:44 +0200

    Primera subida. Realizados hasta el ejercicio 5

--- app/src/main/java/com/aplimoviles/kotlinconsoletest/TopLevelPrint.kt
+++ app/src/main/java/com/aplimoviles/kotlinconsoletest/TopLevelPrint.kt
@@ -2,12 +2,12 @@ package com.aplimoviles.kotlinconsoletest
 
 fun topLevelPrint() = println("Función de nivel superior")
 
-fun learning()= println("I'm\nlearning\nKotlin!")
+fun learning() = println("I'm\nlearning\nKotlin!")
 
 var edad = 24
 const val PI: Float = kotlin.math.PI.toFloat()
 
-fun numerines(){
+fun numerines() {
     println(edad)
     println(PI)
 }
@@ -23,8 +23,29 @@ fun imprimeNombre() {
 
 var arrayEnteros = arrayOf(1, 2, 3, 4, 5)
 
+var arrayEnterosElectricBoogalow = Array(5) {it+1}
+
 fun imprimeArrayDouble() {
-    for(numeros in arrayEnteros) {
+    for (numeros in arrayEnterosElectricBoogalow) {
         print("${numeros.toDouble()} ")
     }
+}
+
+fun enteroReal(num: Int) {
+    var respuesta: String
+    if (num > 0) respuesta = "El $num es positivo"
+    if (num < 0) respuesta = "El $num es negativo" else respuesta = "Es cero ($num)"
+    println(respuesta);
+}
+
+fun geroMomento(num: Int) {
+    var respuesta = when (num > 0) {
+        true -> "El $num es positivo"
+        false -> when (num < 0) {
+            true -> "El $num es negativo"
+            false -> "El $num es cero (duh)"
+        }
+    }
+    println(respuesta);
+
 }
